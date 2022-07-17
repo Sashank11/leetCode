8b85ca50df56fdaa82f876a88749129f44a65812
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-17 11:31:41 +0530

    Valid sudoku

---
+++ Arrays_Hashing/ValidSudoku.java
@@ -0,0 +1,19 @@
+package Arrays_Hashing;
+import java.util.*;
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet();
+        for(int i = 0; i<9; i ++){
+            for(int j = 0; j < 9; j++){
+                char current = board[i][j];
+                if(current !='.'){
+                if(!seen.add(current + "found in row" + i) || 
+                    !seen.add(current + "found in column" + j) || 
+                    !seen.add(current + " found in box" + i/3 + "-" + j/3))
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
