873f1c2b92a8bc2940946676dc2157723a54a57b
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-19 21:51:15 +0530

    ContainerMostWater

---
+++ Two_Pointers/ContainsMostWater.java
@@ -0,0 +1,21 @@
+package Two_Pointers;
+import java.util.*;
+public class ContainsMostWater {
+    public int maxArea(int[] height) {
+        int area = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while( i < j){
+            if( height[i]  < height[j]){
+                area = Math.max(area, height[i] * (j - i));
+                i++;
+            }
+            else{
+                area = Math.max(area, height[j] * (j - i));
+                j--;
+            }
+        }
+        return area;
+
+    }
+}
