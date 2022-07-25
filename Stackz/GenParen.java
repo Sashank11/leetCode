58e07dc39efc94cc3aacdc7020d5df054db55703
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-25 23:33:56 +0530

    init

---
+++ Stackz/GenParen.java
@@ -0,0 +1,9 @@
+package Stackz;
+import java.util.*;
+public class GenParen {
+    public List<String> generateParenthesis(int n) {
+        Stack<Character> bracs = new Stack<>();
+        List<String> result = new ArrayList<>();
+        
+    }
+}
