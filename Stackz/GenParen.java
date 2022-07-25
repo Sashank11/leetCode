0ae0943d3eaf7c6190f48ab3538a1e2541f712a1
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-26 00:42:03 +0530

    Generate Parentheses

--- Stackz/GenParen.java
+++ Stackz/GenParen.java
@@ -2,8 +2,17 @@ package Stackz;
 import java.util.*;
 public class GenParen {
     public List<String> generateParenthesis(int n) {
-        Stack<Character> bracs = new Stack<>();
         List<String> result = new ArrayList<>();
-        
+        backtrack(result, "",0, 0 ,n);
+        return result;
+
+    }
+    public void backtrack(List<String> result, String current_string, int open, int close, int max) {
+        if(current_string.length() == max * 2){  // some base case
+            result.add(current_string);
+        }
+        if(open < max) backtrack(result, current_string + "(", open + 1, close, max);
+        if(close < open) backtrack(result, current_string + ")", open, close + 1, max);
+
     }
 }
