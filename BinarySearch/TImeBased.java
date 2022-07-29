b9d6209a695ddd27440a5b456753c0518baeed8c
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-29 16:47:47 +0530

    Linked List saga

---
+++ BinarySearch/TImeBased.java
@@ -0,0 +1,15 @@
+package BinarySearch;
+
+public class TImeBased {
+    public TimeMap() {
+        
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        
+    }
+    
+    public String get(String key, int timestamp) {
+        
+    }
+}
