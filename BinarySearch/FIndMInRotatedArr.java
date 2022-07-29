c0bc70a6d414cf08b3275a922986806b18763cee
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-29 12:24:28 +0530

    working solution

--- BinarySearch/FIndMInRotatedArr.java
+++ BinarySearch/FIndMInRotatedArr.java
@@ -1,7 +1,28 @@
 package BinarySearch;
 
 public class FIndMInRotatedArr {
-    public int findMin(int[] nums) {
-        
+    public static int findMin(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int l = 0;
+        int r = nums.length -1;
+        int mid = (l + r)/ 2;
+        while(l <=r){
+            mid = (l + r)/2;
+            if(nums[mid] >= nums[0]){
+                min = Math.min(min, nums[mid]);
+                min = Math.min(min, nums[l]);
+                l = mid + 1;
+            }
+            else{
+                    min = Math.min(min, nums[mid]);
+                    min = Math.min(min, nums[l]);
+                    r = mid - 1;
+                }
+            }
+        return min; 
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 0, 1, 2};
+        System.out.println(findMin(arr));
     }
 }
