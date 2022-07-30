2c3db763c2360012724d7ad71f3067b7c6419646
Sashank Rijal <rijalsashank.11@gmail.com>
2022-07-30 11:29:44 +0530

    Reverse Linkedlist

--- LinkedList/RevLinkedlist.java
+++ LinkedList/RevLinkedlist.java
@@ -2,6 +2,20 @@ package LinkedList;
 
 public class RevLinkedlist {
     public ListNode reverseList(ListNode head) {
-        
+        // ListNode one = new ListNode(3);
+        if(head == null){
+            return null;
+        }
+        ListNode curr = head;
+        ListNode temp = null;
+        ListNode prev = null;
+        while(curr != null){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            
+        }
+        return prev;
     }
 }
