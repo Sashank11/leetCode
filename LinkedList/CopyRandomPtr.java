3f8f52e6575ba6e2bec061055474da46fe1858cf
Sashank Rijal <rijalsashank.11@gmail.com>
2022-08-11 23:58:17 +0530

    wrong output attemp1

--- LinkedList/CopyRandomPtr.java
+++ LinkedList/CopyRandomPtr.java
@@ -2,5 +2,24 @@ package LinkedList;
 
 public class CopyRandomPtr {
     public Node copyRandomList(Node head) {
+        HashMap<Node,Node> map = new HashMap<>();
+        Node curr = head;
+        while(curr != null){
+        Node newNode = new Node(curr.val);
+        map.put(curr,newNode);
+        curr = curr.next;
+        }
+        Node root = new Node(0);
+        root.next = map.get(head);
+        curr = head;
+        while(curr!= null){
+            Node newNode = map.get(curr);
+            newNode.next = map.get(curr.next);
+            newNode.random = map.get(curr.next);
+
+            curr =curr.next;
+        }
+    
+        return root.next;
     }
 }
