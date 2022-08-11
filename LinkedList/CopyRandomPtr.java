7ff03bdfe0bac878434b1e91d3fcdd0414b49cb4
Sashank Rijal <rijalsashank.11@gmail.com>
2022-08-12 00:02:05 +0530

    Copy List with Random Pointer

--- LinkedList/CopyRandomPtr.java
+++ LinkedList/CopyRandomPtr.java
@@ -1,25 +1,23 @@
 package LinkedList;
-
+import java.util.*;
 public class CopyRandomPtr {
     public Node copyRandomList(Node head) {
         HashMap<Node,Node> map = new HashMap<>();
+        map.put(null,null);
         Node curr = head;
         while(curr != null){
         Node newNode = new Node(curr.val);
         map.put(curr,newNode);
         curr = curr.next;
         }
-        Node root = new Node(0);
-        root.next = map.get(head);
         curr = head;
         while(curr!= null){
             Node newNode = map.get(curr);
             newNode.next = map.get(curr.next);
-            newNode.random = map.get(curr.next);
-
+            newNode.random = map.get(curr.random);
             curr =curr.next;
         }
     
-        return root.next;
+        return map.get(head);
     }
 }
