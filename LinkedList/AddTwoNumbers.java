3c5ec56f4e26c49dbed7f8287cdb092701167ec7
Sashank Rijal <rijalsashank.11@gmail.com>
2022-08-12 12:06:45 +0530

    TLE soln

--- LinkedList/AddTwoNumbers.java
+++ LinkedList/AddTwoNumbers.java
@@ -1,7 +1,7 @@
 package LinkedList;
 import java.util.*;
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         List<Integer> vals = new ArrayList<>();
         int rem = 0;
         ListNode temp1 = l1;
@@ -31,9 +31,79 @@ public class AddTwoNumbers {
         }
         temp1 = l1;
         temp2 = l2;
-        
+        ListNode root = new ListNode(0);
+        ListNode prev = new ListNode(0);
+        root.next = prev;
+        int i = 0;
         while(temp1 != null || temp2 != null){
-            
+            ListNode newNode = new ListNode(vals.get(i));
+            prev.next = newNode;
+            prev = newNode;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+            i++;
         }
+        return root.next.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        ListNode l1  = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        head.next = l1;
+        l1.next = l2;
+        // l2.next = l3;
+        l3.next = l4;
+        // l4.next = l5;
+        // l5.next = null;
+        System.out.print(addTwoNumbers(l1, l3).next.val);
+        // System.out.println(.val);
     }
 }
+// TLE
+//  List<Integer> vals = new ArrayList<>();
+    //     int rem = 0;
+    //     ListNode temp1 = l1;
+    //     ListNode temp2 = l2;
+    //     while(temp1 != null || temp2 != null){
+    //         int sum = temp1.val + temp2.val;
+    //         if(sum >= 10){
+    //             int dig = sum % 10;
+    //             vals.add(dig);
+    //             rem = 1;
+    //         }
+    //         else{
+    //         sum = temp1.val + temp2.val + rem;
+    //         if(sum >= 10){
+    //             int dig = sum % 10;
+    //             vals.add(dig);
+    //             rem = 1;
+    //         }
+    //         else{
+    //             vals.add(sum);
+    //         }
+    //         rem = 0;
+    //         temp1 = temp1.next;
+    //         temp2 = temp2.next;
+    //         }
+            
+    //     }
+    //     temp1 = l1;
+    //     temp2 = l2;
+    //     ListNode root = new ListNode(0);
+    //     ListNode prev = new ListNode(0);
+    //     root.next = prev;
+    //     int i = 0;
+    //     while(temp1 != null || temp2 != null){
+    //         ListNode newNode = new ListNode(vals.get(i));
+    //         prev.next = newNode;
+    //         prev = newNode;
+    //         temp1 = temp1.next;
+    //         temp2 = temp2.next;
+    //         i++;
+    //     }
+    //     return root.next.next;
+    // }
