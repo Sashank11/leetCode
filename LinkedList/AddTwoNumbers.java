ada17e517c38364e499df2fa3ff04e9583817387
Sashank Rijal <rijalsashank.11@gmail.com>
2022-08-12 13:38:59 +0530

    Add Two Numbers

--- LinkedList/AddTwoNumbers.java
+++ LinkedList/AddTwoNumbers.java
@@ -2,46 +2,36 @@ package LinkedList;
 import java.util.*;
 public class AddTwoNumbers {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        List<Integer> vals = new ArrayList<>();
-        int rem = 0;
-        ListNode temp1 = l1;
-        ListNode temp2 = l2;
-        while(temp1 != null || temp2 != null){
-            int sum = temp1.val + temp2.val;
-            if(sum >= 10){
-                int dig = sum % 10;
-                vals.add(dig);
-                rem = 1;
-            }
-            else{
-            sum = temp1.val + temp2.val + rem;
-            if(sum >= 10){
-                int dig = sum % 10;
-                vals.add(dig);
-                rem = 1;
-            }
-            else{
-                vals.add(sum);
-            }
-            rem = 0;
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-            }
-            
-        }
-        temp1 = l1;
-        temp2 = l2;
         ListNode root = new ListNode(0);
-        ListNode prev = new ListNode(0);
-        root.next = prev;
-        int i = 0;
-        while(temp1 != null || temp2 != null){
-            ListNode newNode = new ListNode(vals.get(i));
-            prev.next = newNode;
-            prev = newNode;
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-            i++;
+        ListNode curr = root;
+        int carry = 0;
+        int v1 = 0;
+        int v2 = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            if (l1 != null)
+                v1 = l1.val;
+            else 
+                v1 = 0;
+            if (l2 != null)
+                v2 = l2.val;
+            else 
+                v2 = 0;
+            int val = v1 + v2 + carry;
+            carry = val / 10;
+            val = val % 10;
+            curr.next = new ListNode(val);
+            
+            curr = curr.next;
+            if (l1 != null)
+                l1 = l1.next;
+            else
+                l1 = null;
+            if (l2 != null)
+                l2 = l2.next;
+            else
+                l2 = null;
+        }
+        return root.next;
         }
         return root.next.next;
     }
