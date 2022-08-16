0d31043c37ecd9132aef683f8c9c0412a603cdee
Sashank Rijal <rijalsashank.11@gmail.com>
2022-08-16 10:28:06 +0530

    Linked List Cycle

--- LinkedList/LLcycle.java
+++ LinkedList/LLcycle.java
@@ -2,15 +2,17 @@ package LinkedList;
 import java.util.*;
 public class LLcycle {
     public boolean hasCycle(ListNode head) {
-        ListNode curr = head;
-        HashSet<ListNode> lvals = new HashSet<>();
-        while(curr != null){
-            if(lvals.contains(curr)){
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null || head.next  == null){
+            return false;
+        }
+        while(fast != null && fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
                 return true;
             }
-            lvals.add(curr);
-            curr = curr.next;
-            
         }
         return false;
         
