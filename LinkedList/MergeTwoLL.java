8c064079803815cd03c00019890753771188fac3
Sashank Rijal <rijalsashank.11@gmail.com>
2022-09-09 09:36:07 +0545

    revise  mergeLL

--- LinkedList/MergeTwoLL.java
+++ LinkedList/MergeTwoLL.java
@@ -24,3 +24,26 @@ public class MergeTwoLL {
         return dolt.next; 
     }
 }
+// revise
+// ListNode root = new ListNode(0);
+//         ListNode tail = root;
+//         while(list1 != null && list2 != null){
+//             if(list1.val < list2.val){
+//                 tail.next = list1;
+//                 list1 = list1.next;
+//             }
+//             else{
+//                 tail.next = list2;
+//                 list2 = list2.next;
+//             }
+//             tail = tail.next;
+//         }
+        
+//         if(list1 != null){
+//             tail.next = list1;
+//         }
+//         if(list2 != null){
+//             tail.next = list2;
+//         }
+        
+//         return root.next;
