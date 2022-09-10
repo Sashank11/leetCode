e833ddd33025120e2d1ae67fec0c1c30346b97e9
Sashank Rijal <rijalsashank.11@gmail.com>
2022-09-10 23:36:35 +0545

    revise addLL

--- LinkedList/AddTwoNumbers.java
+++ LinkedList/AddTwoNumbers.java
@@ -95,3 +95,40 @@ public class AddTwoNumbers {
     //     }
     //     return root.next.next;
     // }
+
+    // revise
+    
+    // ListNode dummy = new ListNode(0);
+    //     ListNode curr = dummy;
+    //     dummy.next = curr;
+    //     int carry = 0;
+    //     int v1, v2;
+    //     while(l1 != null || l2 != null || carry != 0){
+    //         if(l1 != null){
+                
+    //             v1 = l1.val;
+    //             }
+    //         else{
+    //             v1 = 0;
+    //         }
+            
+    //         if(l2 != null){
+                
+    //             v2 = l2.val;
+    //             }
+    //         else{
+    //             v2 = 0;
+    //         }
+    //         int sum = v1 + v2 + carry;
+    //         carry = sum / 10;
+    //         sum = sum % 10;
+    //         curr.next = new ListNode(sum);
+    //         curr = curr.next;
+    //         if(l1 != null){
+    //         l1 = l1.next;
+    //         }
+    //         if(l2 != null){
+    //         l2 = l2.next;
+    //         }
+    //     }
+    //     return dummy.next;
