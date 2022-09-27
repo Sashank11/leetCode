f8d8438727baef7d34f8653ac33b04d5ec557117
Sashank Rijal <rijalsashank.11@gmail.com>
2022-09-27 11:13:21 +0545

    Sub trees

---
+++ Trees/Subtree.java
@@ -0,0 +1,25 @@
+package Trees;
+public class Subtree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if(subRoot == null){
+            return true;
+        }
+        if(root == null){
+            return false;
+        }
+        if (sameTree(root, subRoot) == true){
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+        public boolean sameTree(TreeNode root, TreeNode subRoot){
+            if(root == null && subRoot == null){
+                return true;
+            }
+            if(root != null && subRoot != null && root.val == subRoot.val){
+                return  sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right);
+            }
+                return false;
+        }
+}
