121ae0a1362487a84bb925b4cdb445e42467b14b
Sashank Rijal <rijalsashank.11@gmail.com>
2022-10-14 10:53:19 +0545

    attempt 1

--- Trees/ValidBST.java
+++ Trees/ValidBST.java
@@ -3,3 +3,30 @@ import java.util.*;
 public class ValidBST {
     
 }
+// 62 cases
+// if(root == null){
+//             return true;
+//         }
+//         if(root.left == null && root.right == null){
+//             return true;
+//         }
+//         if(root.left == null){
+//             if((root.val < root.right.val)){
+//             return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         if(root.right == null){
+//             if((root.val > root.left.val)){
+//             return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         if(!(root.val > root.left.val && root.val < root.right.val)){
+//             return false;
+//         }
+//         return isValidBST(root.left) && isValidBST(root.right);
