7438387d05e56d6283348c21b735df7ba046ff91
Sashank Rijal <rijalsashank.11@gmail.com>
2022-10-22 22:40:55 +0545

    Contruct tree

--- Trees/ConsTree.java
+++ Trees/ConsTree.java
@@ -13,6 +13,7 @@ public TreeNode buildTree(int[] preorder, int[] inorder) {
             }
         }
         root.left = buildTree(Arrays.copyOfRange(preorder, 1, mid + 1), Arrays.copyOfRange(inorder,0,mid));
+        
         root.right = buildTree(Arrays.copyOfRange(preorder,mid+1, preorder.length), Arrays.copyOfRange(inorder,mid + 1, inorder.length));
         return root;
     }
