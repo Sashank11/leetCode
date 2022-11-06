e20121adffba959339af71dddaf2cc04b7fcd8d1
Sashank Rijal <rijalsashank.11@gmail.com>
2022-11-06 17:56:10 +0545

    kth largest

--- heap_pri/Kth_large.java
+++ heap_pri/Kth_large.java
@@ -1,5 +1,23 @@
 package heap_pri;
-
+import java.util.*;
 public class Kth_large {
+    final PriorityQueue<Integer> heap = new PriorityQueue<>();
+    final int k;
+    public void KthLargest(int k, int[] nums) {
+        this.k = k;
+        for(int n : nums){
+            add(n);
+        }
+    }
     
+    public int add(int val) {
+        if(heap.size() < k){
+            heap.offer(val);
+        }
+        else if(val > heap.peek()){
+            heap.poll();
+            heap.add(val);
+        }
+        return heap.peek();
+    }
 }
