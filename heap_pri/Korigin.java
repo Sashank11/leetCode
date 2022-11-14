c44eb58c8c6e6d838d094395c285ade3240aa2b7
Sashank Rijal <rijalsashank.11@gmail.com>
2022-11-14 11:12:50 +0545

    K Closest Points to Origin

--- heap_pri/Korigin.java
+++ heap_pri/Korigin.java
@@ -1,8 +1,24 @@
 package heap_pri;
 import java.util.*;
 public class Korigin{
-public int[][] kClosest(int[][] points, int k) {
-    
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) ->
+        Integer.compare((b[0] * b[0] + b[1] * b[1]),
+                        (a[0] * a[0] + a[1] * a[1])));
+        for(int[] point : points){
+            heap.add(point);
+            if(heap.size() > k){
+                heap.remove();
+            }
+        }
+
+        int[][] ans = new int[k][2];
+        for(int i =0; i< k; i++){
+            int cur[] = heap.poll();
+            ans[i][0] = cur[0];
+            ans[i][1] = cur[1];
+        }
+        return ans;
         
     }
 }
